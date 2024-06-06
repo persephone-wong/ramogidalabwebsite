cffccbdbe1cd51465456880212e2400821e62dbc
persephone-wong <persephone.w315@gmail.com>
2024-06-06 11:47:39 -0700

    add basic structure to website

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+let ejs = require('ejs');
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.listen(3000, () => {
+    console.log('server started');
+});
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get('/research', (req, res) => {
+    res.render('research.ejs');
+});
+
+app.get('/people', (req, res) => {
+    res.render('people.ejs');
+});
+
+app.get('/publications', (req, res) => {
+    res.render('people.ejs');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact.ejs');
+});
+
+app.get('/news', (req, res) => {
+    res.render('news.ejs');
+});
+
+app.get('*', (req, res) => {
+    res.status(404).render('error.ejs');
+});
+
+
+
