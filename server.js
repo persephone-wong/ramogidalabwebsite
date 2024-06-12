71a9946a174131c0fb6f3f2acd0e6906efb63b73
persephone-wong <persephone.w315@gmail.com>
2024-06-12 12:40:51 -0700

    add session tracking

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 let ejs = require('ejs');
 app.use(express.static('public'));
+var session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+require('dotenv').config()
+
+
+
+var store = new MongoDBStore({
+    uri: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bzl2ogl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+    collection: 'WebSessions'
+});
+
 
 app.set('view engine', 'ejs');
 
@@ -14,6 +25,23 @@ express.static('public');
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.post('/dashboard', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log(username, password);
+    if (username === 'admin' && password === 'admin') {
+        res.render('dashboard.ejs');
+    } else {
+        res.send("Invalid credentials");
+    }
+
+});
+
 
 app.get('/research', (req, res) => {
     res.render('research.ejs');
