c60311299e6b74d3b93d01dd9b4f2d0e3e52ae7c
persephone-wong <persephone.w315@gmail.com>
2024-06-24 16:45:24 -0700

    scroll function changes view

--- public/scripts/research.js
+++ public/scripts/research.js
@@ -19,40 +19,42 @@ scrollFunction = debounce (function () {
     let currentScrollPos = window.scrollY || document.documentElement.scrollTop;
     if (currentScrollPos > lastScrollPos) {
         scrolls++;
+        console.log(scrolls);
         changeResearch();
     } else {
         if (scrolls > 0) {
         scrolls--;}
+        console.log(scrolls);
         changeResearch();
     }
     lastScrollPos = currentScrollPos <= 0 ? 0 : currentScrollPos;
 
-},100);
+},500);
 
 changeResearch = function () {
     if (scrolls === 0) {
-        document.getElementById('research_intro').style.display = 'flex';
+        document.getElementById('research_intro').style.display = 'block';
         document.getElementById('research_1').style.display = 'none';
         document.getElementById('research_2').style.display = 'none';
         document.getElementById('research_3').style.display = 'none';
     }
     if (scrolls === 1) {
         document.getElementById('research_intro').style.display = 'none';
-        document.getElementById('research_1').style.display = 'flex';
+        document.getElementById('research_1').style.display = 'block';
         document.getElementById('research_2').style.display = 'none';
         document.getElementById('research_3').style.display = 'none';
     }
     if (scrolls === 2) {
         document.getElementById('research_intro').style.display = 'none';
         document.getElementById('research_1').style.display = 'none';
-        document.getElementById('research_2').style.display = 'flex';
+        document.getElementById('research_2').style.display = 'block';
         document.getElementById('research_3').style.display = 'none';
     }
     if (scrolls === 3) {
         document.getElementById('research_intro').style.display = 'none';
         document.getElementById('research_1').style.display = 'none';
         document.getElementById('research_2').style.display = 'none';
-        document.getElementById('research_3').style.display = 'flex';
+        document.getElementById('research_3').style.display = 'block';
     }
 };
 
