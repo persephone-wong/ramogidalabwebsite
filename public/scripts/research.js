da0fcdc4dd2a0198f30b4137f8d2e257ffcef59f
persephone-wong <persephone.w315@gmail.com>
2025-06-08 20:38:55 -0700

    fixed research menu

--- public/scripts/research.js
+++ public/scripts/research.js
@@ -1,99 +1,94 @@
 let scrolls = 0;
-let lastScrollPos = 0;
 let shouldChangeResearch = true;
-
-
-function debounce(func, wait) {
-    let timeout;
-    return function executedFunction() {
-        const later = function() {
-            clearTimeout(timeout);
-            func();
-        };
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-    };
-};
-
-scrollFunction = debounce(function () {
-    if (!shouldChangeResearch) {
-        return;
-    };
-
-    let currentScrollPos = window.scrollY || document.documentElement.scrollTop;
-    if (currentScrollPos > lastScrollPos) {
-        if (scrolls < 3) {
-        scrolls++;
-        console.log(scrolls);
-        changeResearch();}
-    } else if (currentScrollPos < lastScrollPos) {
-        if (scrolls > 0) {
-            scrolls--;
-            changeResearch();
-        }        
-    }
-    if (currentScrollPos = lastScrollPos) {
-        return;
-    }
-    lastScrollPos = currentScrollPos <= 0 ? 0 : currentScrollPos;
-    shouldChangeResearch = false;
-    setTimeout(() => {
-        shouldChangeResearch = true;
-    }, 500);
-}, 100);
 
 changeResearch = function () {
     if (scrolls === 0) {
         document.getElementById('research_intro').style.display = 'block';
-        document.getElementById('research_dot_intro').style.display = 'block';
+        document.getElementById('research_dot_0_blank').style.display = 'none';
+        document.getElementById('research_dot_0_filled').style.display = 'block';
         document.getElementById('research_1').style.display = 'none';
-        document.getElementById('research_dot_1').style.display = 'none';
+        document.getElementById('research_dot_1_blank').style.display = 'block';
+        document.getElementById('research_dot_1_filled').style.display = 'none';
         document.getElementById('research_2').style.display = 'none';
-        document.getElementById('research_dot_2').style.display = 'none';
+        document.getElementById('research_dot_2_blank').style.display = 'block';
+        document.getElementById('research_dot_2_filled').style.display = 'none';
         document.getElementById('research_3').style.display = 'none';
-        document.getElementById('research_dot_3').style.display = 'none';
+        document.getElementById('research_dot_3_blank').style.display = 'block';
+        document.getElementById('research_dot_3_filled').style.display = 'none';
         
     }
     if (scrolls === 1) {
         document.getElementById('research_intro').style.display = 'none';
-        document.getElementById('research_dot_intro').style.display = 'none';
+        document.getElementById('research_dot_0_blank').style.display = 'block';
+        document.getElementById('research_dot_0_filled').style.display = 'none';
         document.getElementById('research_1').style.display = 'block';
-        document.getElementById('research_dot_1').style.display = 'block';
+        document.getElementById('research_dot_1_blank').style.display = 'none';
+        document.getElementById('research_dot_1_filled').style.display = 'block';
         document.getElementById('research_2').style.display = 'none';
-        document.getElementById('research_dot_2').style.display = 'none';
+        document.getElementById('research_dot_2_blank').style.display = 'block';
+        document.getElementById('research_dot_2_filled').style.display = 'none';
         document.getElementById('research_3').style.display = 'none';
-        document.getElementById('research_dot_3').style.display = 'none';
+        document.getElementById('research_dot_3_blank').style.display = 'block';
+        document.getElementById('research_dot_3_filled').style.display = 'none';
     }
     if (scrolls === 2) {
         document.getElementById('research_intro').style.display = 'none';
-        document.getElementById('research_dot_intro').style.display = 'none';
+        document.getElementById('research_dot_0_blank').style.display = 'block';
+        document.getElementById('research_dot_0_filled').style.display = 'none';
         document.getElementById('research_1').style.display = 'none';
-        document.getElementById('research_dot_1').style.display = 'none';
+        document.getElementById('research_dot_1_blank').style.display = 'block';
+        document.getElementById('research_dot_1_filled').style.display = 'none';
         document.getElementById('research_2').style.display = 'block';
-        document.getElementById('research_dot_2').style.display = 'block';
+        document.getElementById('research_dot_2_blank').style.display = 'none';
+        document.getElementById('research_dot_2_filled').style.display = 'block';
         document.getElementById('research_3').style.display = 'none';
-        document.getElementById('research_dot_3').style.display = 'none';
+        document.getElementById('research_dot_3_blank').style.display = 'block';
+        document.getElementById('research_dot_3_filled').style.display = 'none';
     }
     if (scrolls === 3) {
         document.getElementById('research_intro').style.display = 'none';
-        document.getElementById('research_dot_intro').style.display = 'none';
+        document.getElementById('research_dot_0_blank').style.display = 'block';
+        document.getElementById('research_dot_0_filled').style.display = 'none';
         document.getElementById('research_1').style.display = 'none';
-        document.getElementById('research_dot_1').style.display = 'none';
+        document.getElementById('research_dot_1_blank').style.display = 'block';
+        document.getElementById('research_dot_1_filled').style.display = 'none';
         document.getElementById('research_2').style.display = 'none';
-        document.getElementById('research_dot_2').style.display = 'none';
+        document.getElementById('research_dot_2_blank').style.display = 'block';
+        document.getElementById('research_dot_2_filled').style.display = 'none';
         document.getElementById('research_3').style.display = 'block';
-        document.getElementById('research_dot_3').style.display = 'block';
+        document.getElementById('research_dot_3_blank').style.display = 'none';
+        document.getElementById('research_dot_3_filled').style.display = 'block';
     }
-    shouldChangeResearch = false;
-    window.scrollTo(0, 100);
-    lastScrollPos = window.scrollY
-    setTimeout(() => {
-        shouldChangeResearch = true;
-    }, 500);
+}
 
 
+function setupDotClicks() {
+    document.getElementById('research_dot_0_blank').onclick = function() {
+        scrolls = 0;
+        console.log("clicked")
+        changeResearch();
+    };
+    document.getElementById('research_dot_1_blank').onclick = function() {
+        scrolls = 1;
+        console.log("clicked")
+        changeResearch();
+    };
+    document.getElementById('research_dot_2_blank').onclick = function() {
+        scrolls = 2;
+        console.log("clicked")
+        changeResearch()
+        ;
+    };
+    document.getElementById('research_dot_3_blank').onclick = function() {
+        scrolls = 3;
+        console.log("clicked")
+        changeResearch();
+    };
+}
+
+
+
+window.onload = function() {
+    setupDotClicks();
 };
 
-
-
-window.onscroll = scrollFunction;
